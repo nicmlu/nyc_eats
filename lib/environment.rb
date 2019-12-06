aa7d2f753eafc727063bf1668dcaf57730183370
nicmlu <NicoleMLucien@gmail.com>
2019-12-05 21:58:27 -0500

    updated environment file

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,9 @@
+require "httparty"
+require "pry"
 require "nyc_eats/version"
+require "nyc_eats/api"
+require "nyc_eats/cli"
+require "nyc_eats/restaurant"
 
 module NycEats
   class Error < StandardError; end
